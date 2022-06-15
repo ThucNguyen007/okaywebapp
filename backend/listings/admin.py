8546b2a1cd8ffdc802188ef157a0d00eb2d0535e
Thuc Nguyen <thucdlnguyen@gmail.com>
2022-06-14 22:44:22 -0400

    First commit

---
+++ backend/listings/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from listings.models import Listing
+from listings.models import Poi
+from .forms import PoisForm
+
+# Register your models here.
+class PoiAdmin(admin.ModelAdmin):
+    form = PoisForm
+
+admin.site.register(Listing)
+admin.site.register(Poi, PoiAdmin)
